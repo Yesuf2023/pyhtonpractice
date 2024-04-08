76d314b6c6b15157324024c7a7db34a66bbb8601
Mohammed <12001003714105@FEDIDCARD.GOV>
2024-04-08 17:35:03 -0500

    fourht commit

---
+++ tax.py
@@ -0,0 +1,6 @@
+gross= 1200
+tax_rate= 0.15
+tax = gross * tax_rate
+net = gross - tax
+message = "The net pay is" + " " + repr(net) + ", " + "please make sure you pay your apt rent and gorcery before you run outoff money. You are grown man, do not come to your mom please to eat cereal!"
+print(message)
