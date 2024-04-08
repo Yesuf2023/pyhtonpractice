38fc9990d27df9ddc25886acdc8b2eb571e328f9
Mohammed <12001003714105@FEDIDCARD.GOV>
2024-04-08 17:18:50 -0500

    the third commit

---
+++ add.py
@@ -0,0 +1,15 @@
+#Calculate 20% tip
+price = 200
+tip = price * 0.2
+m = price + tip
+message = "The total is" + " " + repr(m) + ", " + "please bring your awesome family back, we cook our food with love and passion!"
+print(message)
+#Calculate the area of a circle
+radius = 10
+area = 3.142 * radius ** 2
+print(area)
+#Calculate the area of a triangle
+base = 20
+height = 30
+area = 0.5 * base * height
+print(area)
